e9265c33ad932482744fe72a47e4bd03c1e88f2a
fenshitianyue <abzanda@163.com>
2019-11-24 18:01:21 +0800

    添加视图模块代码

---
+++ view/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class View(object):
+    methods = None
+    methods_meta = None
+
+    def dispath_request(self, request, *args, **kwargs):
+        """
+        视图处理函数调度入口
+        """
+        raise NotImplementedError
+
+    @classmethod
+    def get_func(cls, name):
+        """
+        生成视图处理函数
+        """
+        def func(*args, **kwargs):
+            obj = func.view_class()
+            return obj.dispath_request(*args, **kwargs)
+
+        func.view_class = cls
+        func.__name__ = name
+        func.__doc__ = cls.__doc__
+        func.__module__ = cls.__module__
+        func.methods = cls.methods
+        return func
