dbb7e0d5a06d1d00775c02b6ed140cf97644887c
fenshitianyue <abzanda@163.com>
2019-12-24 23:08:30 +0800

    update code

--- core/database.py
+++ core/database.py
@@ -9,5 +9,29 @@ DB_USER = 'root'
 DB_PASSWD = ''
 DB_DATABASE = 'test'
 
+try:
+    db_conn = BaseDB(DB_USER, DB_PASSWD, DB_DATABASE)
+except Exception as e:
+    code, __ = e.args  # 获取异常代码
+    if 1049 == code:
+        create_table = """
+        create table user(
+        id int primary key auto_increment,
+        name varchar(50) unique
+        ) charset=utf8
+        """
+        db_conn = BaseDB(DB_USER, DB_PASSWD)
+        ret = db_conn.create_db(DB_DATABASE)
+        if ret.is_ok:
+            ret = db_conn.choose_db(DB_DATABASE)
+            if ret.is_ok:
+                ret = db_conn.execute(create_table)
+        if not ret.is_ok:
+            db_conn.drop_db(DB_DATABASE)
 
+            print ret.error.args
+            exit()
+    else:
+        print e
+        exit()
 
