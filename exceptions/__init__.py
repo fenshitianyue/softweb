47569aa7c03ff1fc7c4e904d00d32df5e7b0b395
fenshitianyue <abzanda@163.com>
2019-12-25 11:06:43 +0800

    update exception module

--- exceptions/__init__.py
+++ exceptions/__init__.py
@@ -4,7 +4,7 @@ class SoftWebExecption(object):
     """
     框架异常基类
     """
-    def __init__(self, code=500, message='Error'):
+    def __init__(self, code=500, message='Invild server error'):
         self.code = code
         self.message = message
 
@@ -12,11 +12,44 @@ class SoftWebExecption(object):
         return self.message
 
 
-class URLExistError(SoftWebExecption):
-    def __init__(self, message='URL exists.'):
-        super(URLExistError, self).__init__(message)
+# class URLExistError(SoftWebExecption):
+#     """访问的URL不存在"""
+#     def __init__(self, message='URL exists.'):
+#         super(URLExistError, self).__init__(message)
 
 
 class EndpointExistError(SoftWebExecption):
-    def __init__(self, message='Endpoint exists.'):
-        super(EndpointExistError, self).__init__(message)
+    """端点错误"""
+    def __init__(self, code=400, message='Endpoint not exists.'):
+        super(EndpointExistError, self).__init__(code, message)
+
+
+class FileNoExistsError(SoftWebExecption):
+    """文件不存在"""
+    def __init__(self, code=2, message='File not found'):
+        super(FileNoExistsError, self).__init__(code, message)
+
+
+class RequireReadPermissionError(SoftWebExecption):
+    """权限不足"""
+    def __init__(self, code=13, message='Require read permission'):
+        super(RequireReadPermissionError, self).__init__(code, message)
+
+
+class InvaildRequestMethodError(SoftWebExecption):
+    """错误的请求方式"""
+    def __init__(self, code=405, message='Unknow or unsupported request method'):
+        super(InvaildRequestMethodError, self).__init__(code, message)
+
+
+class PageNotFoundError(SoftWebExecption):
+    """页面未找到"""
+    def __init__(self, code=404, message='Page not found'):
+        super(PageNotFoundError, self).__init__(code, message)
+
+
+class UnknownError(SoftWebExecption):
+    """未知错误"""
+    def __init__(self, code=500, message='Invild server error'):
+        super(UnknownError, self).__init__(code, message)
+
