7f050f35a0d1539ce41b7e013bcbece984e47aa1
fenshitianyue <abzanda@163.com>
2019-12-25 10:42:11 +0800

    update code

--- core/database.py
+++ core/database.py
@@ -6,8 +6,12 @@ from softweb.dbconn import BaseDB
 # 数据库的相关配置暂时写在这里
 
 DB_USER = 'root'
-DB_PASSWD = ''
+DB_PASSWD = 'nihao.'
 DB_DATABASE = 'test'
+
+# 第一次连接数据库可以预见的异常:
+#   当前连接的数据库还不存在，所以异常处理的流程:
+#   创建数据库、选择新数据库、建表、返回最后一次执行的结果
 
 try:
     db_conn = BaseDB(DB_USER, DB_PASSWD, DB_DATABASE)
